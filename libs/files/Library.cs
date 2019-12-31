d92cc140d8c0e99d6b04af675c70e36b0fc20b52
Dan Larsen <dan@tweng.co>
2019-12-31 11:15:02 -0800

    Expanded SSL a bit: more stuff in math, added files for basic file/directory IO.

---
+++ libs/files/Library.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using Shiro.Interop;
+using Shiro;
+
+namespace files
+{
+	public class Library : ShiroPlugin
+	{
+		public override void RegisterAutoFunctions(Interpreter shiro)
+		{
+            shiro.RegisterAutoFunction("read-file", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                return new Token(File.ReadAllText(fileName));
+            });
+
+            shiro.RegisterAutoFunction("write-file", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+                t = toke.Children[1];
+                var contents = t.Eval(shiro);
+
+                File.WriteAllText(fileName, contents.ToString());
+                return contents;
+            });
+
+            shiro.RegisterAutoFunction("append-file", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+                t = toke.Children[1];
+                var contents = t.Eval(shiro);
+
+                File.AppendAllText(fileName, contents.ToString());
+                return contents;
+            });
+
+            shiro.RegisterAutoFunction("nuke-file", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                if (!File.Exists(fileName))
+                    return Token.False;
+
+                File.Delete(fileName);
+                return Token.True;
+            });
+
+            shiro.RegisterAutoFunction("file?", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                return File.Exists(fileName) ? Token.True : Token.False;
+            });
+
+            shiro.RegisterAutoFunction("dir?", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                return Directory.Exists(fileName) ? Token.True : Token.False;
+            });
+
+            shiro.RegisterAutoFunction("mkdir", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                if (Directory.Exists(fileName))
+                    return Token.False;
+
+                Directory.CreateDirectory(fileName);
+                return Directory.Exists(fileName) ? Token.True : Token.False;
+            });
+
+            shiro.RegisterAutoFunction("rmdir", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                var fileName = t.ToString();
+
+                if (!Directory.Exists(fileName))
+                    return Token.False;
+
+                Directory.Delete(fileName);
+                return Token.True;
+            });
+        }
+	}
+}
--- libs/math/Library.cs
+++ libs/math/Library.cs
@@ -34,10 +34,54 @@ namespace math
                 if (t.Toke is decimal)
                     return new Token(Math.Abs((double)((decimal)t.Toke)).ToString());
 
-                Interpreter.Error("Internal error in abs-- there must be a new numeric type that I didn't handle.");
+                Interpreter.Error("Internal error in abs -- there must be a new numeric type that I didn't handle.");
                 return Token.Nil;
             });
 
+            shiro.RegisterAutoFunction("int", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                if (!t.IsNumeric)
+                    Interpreter.Error("int function requires a numeric value, not " + t.ToString());
+
+                if (t.Toke is long)
+                    return t;
+                if (t.Toke is decimal)
+                    return new Token(((long)(decimal)t.Toke).ToString());
+
+                Interpreter.Error("Internal error in int -- there must be a new numeric type that I didn't handle.");
+                return Token.Nil;
+            });
+
+            shiro.RegisterAutoFunction("int?", (i, toke) =>
+            {
+                var t = toke.Children[0];
+
+                if (t.Toke is long)
+                    return Token.True;
+
+                return Token.False;
+            });
+
+            shiro.RegisterAutoFunction("dec?", (i, toke) =>
+            {
+                var t = toke.Children[0];
+
+                if (t.Toke is decimal)
+                    return Token.True;
+
+                return Token.False;
+            });
+
+            shiro.RegisterAutoFunction("rand", (i, toke) =>
+            {
+                var t = toke.Children[0];
+                if (!t.IsNumeric)
+                    Interpreter.Error("rand function requires a numeric value, not " + t.ToString());
+
+                int upper = (int)t.Toke;
+                return new Token(new Random().Next(0, upper).ToString());
+            });
         }
 	}
 }
